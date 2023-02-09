69a1ec7aee26240ed56a51daa53f2934e08dedcc
crystal.ku <inochiyoshi@hotmail.com>
2023-02-09 23:56:17 +0800

    移除不必要的註解

--- MyWork/SHA1_Case1.aspx.cs
+++ MyWork/SHA1_Case1.aspx.cs
@@ -4,10 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
-//==自己寫的（宣告）==
-using System.Security.Cryptography;   //-- SHA1專用
-using System.Text;                            //-- StringBuilder專用
+using System.Security.Cryptography;   
+using System.Text; // StringBuilder
 
 
 public partial class Book_Sample_B12_Member_Login_Session_SHA1_Case1 : System.Web.UI.Page
@@ -21,7 +19,7 @@ public partial class Book_Sample_B12_Member_Login_Session_SHA1_Case1 : System.We
     {
         string source = TextBox1.Text;
 
-        string hash = GetSHA1Hash(source);  //-- SHA1雜湊運算的 function寫在下面。
+        string hash = GetSHAHash(source);  //-- SHA雜湊運算的 function寫在下面。
 
         Label1.Text = "原本的字串「<font color=blue>" + source + "</font>」，經過SHA1雜湊運算後變成：&nbsp;&nbsp; " + hash;
     
@@ -30,29 +28,23 @@ public partial class Book_Sample_B12_Member_Login_Session_SHA1_Case1 : System.We
 
     //===================================================================
     // 資料來源：http://msdn.microsoft.com/zh-tw/library/system.security.cryptography.sha1.aspx 
-    //
-    //== 底下這部分的程式，就算讀者不懂也可以使用它。照著做就對了。
-    //== 只要知道我們輸入一段字串，它就會用 SHA1進行加密後，傳回「加密後的字串」。
-    protected string GetSHA1Hash(string uInput)
+    
+    protected string GetSHAHash(string uInput)
     {
         ////** 方法一 ****************************************************************
-        SHA256 SHA256Hasher = SHA256.Create();
-        //-- SHA1必須搭配 System.Security.Cryptography命名空間
-
+        //SHA的.ComputeHash(Byte[]) 方法，計算指定位元組陣列的雜湊值。
+        //（字串轉成Byte[]）  System.Text.Encoding.Default.GetBytes(uInput)
+        SHA256 SHA256Hasher = SHA256.Create();//using System.Security.Cryptography
         Byte[] data = SHA256Hasher.ComputeHash(Encoding.Default.GetBytes(uInput));
-        //--SHA1的.ComputeHash(Byte[]) 方法，計算指定位元組陣列的雜湊值。
-        //  （字串轉成Byte[]）  System.Text.Encoding.Default.GetBytes(uInput)
+       
 
 
         //** 方法二 ****************************************************************
         //Byte[] data;
-        //SHA1CryptoServiceProvider sha1SP = new SHA1CryptoServiceProvider();
-        ////-- 使用密碼編譯服務提供者 (CSP) 所提供之實作，計算輸入資料的 SHA1 雜湊值。
-        ////-- http://msdn.microsoft.com/zh-tw/library/system.security.cryptography.sha1cryptoserviceprovider.aspx
+        //SHA1CryptoServiceProvider sha1SP = new SHA1CryptoServiceProvider();        
         //data = sha1SP.ComputeHash(Encoding.Default.GetBytes(uInput));
 
-        StringBuilder sBuilder = new StringBuilder();
-        ////-- StringBuilder必須搭配 System.Text命名空間
+        StringBuilder sBuilder = new StringBuilder();//using System.Text
 
         // Loop through each byte of the hashed data and format each one as a hexadecimal string.
         for (int i = 0; i < data.Length; i++)
